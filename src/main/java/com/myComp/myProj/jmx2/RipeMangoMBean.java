20b19401ea6fec8d606d89362d3a4b3f48af4181
Dibyojyoti Sanyal <Dibyojyoti@users.noreply.github.com>
2021-08-01 15:28:49 +0200

    Update RipeMangoMBean.java

--- src/main/java/com/myComp/myProj/jmx2/RipeMangoMBean.java
+++ src/main/java/com/myComp/myProj/jmx2/RipeMangoMBean.java
@@ -1,4 +1,4 @@
-package com.myComp.myProj.jmx;
+package com.myComp.myProj.jmx2;
 public interface RipeMangoMBean {
  String objectName = "com.myComp.myProj.jmx:type=RipeMango";
  boolean isEdible();
